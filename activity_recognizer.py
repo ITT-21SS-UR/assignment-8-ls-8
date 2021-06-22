d8407b8be098b24273f515ea9f9b1653f3c80986
michischmidt <schmidt.m.dev@gmail.com>
2021-06-22 16:03:49 +0200

    first fft extraction

--- activity_recognizer.py
+++ activity_recognizer.py
@@ -7,24 +7,65 @@ import sys
 from PyQt5 import QtGui, QtCore
 import pyqtgraph as pg
 from pyqtgraph.flowchart import Flowchart, Node
+import numpy as np
 from DIPPID_pyqtnode import DIPPIDNode, BufferNode
 import pyqtgraph.flowchart.library as fclib
 
-def init_accel_nodes(layout, fc, dippid_node):
+
+# custom FFT node for frequency spectrogram output
+class FftNode(Node):
+    nodeName = "FftNode"
+
+    def __init__(self, name):
+        Node.__init__(self, name, terminals={
+            "accelX": dict(io="in"),
+            "accelY": dict(io="in"),
+            "accelZ": dict(io="in"),
+            "frequency": dict(io="out"),
+        })
+
+    def calculate_frequency(self, data):
+        try:
+            n = len(data)
+            # fft computing and normalization and 
+            # use only first half as the function is mirrored
+            frequenzy = np.abs(np.fft.fft(data) / n)[0:int(n / 2)]
+            return frequenzy
+        except Exception as e:
+            print(e)
+
+    def process(self, **kwds):
+        x_frequency = self.calculate_frequency(kwds["accelX"])
+        y_frequency = self.calculate_frequency(kwds["accelY"])
+        z_frequency = self.calculate_frequency(kwds["accelZ"])
+        
+        return {
+            "x": x_frequency,
+            "y": y_frequency,
+            "z": z_frequency,
+        }
+
+
+def init_nodes(layout, fc, dippid_node, fft_node):
     # create buffer nodes
     buffer_node_x = fc.createNode("Buffer", pos=(150, 0))
     buffer_node_y = fc.createNode("Buffer", pos=(150, 100))
     buffer_node_z = fc.createNode("Buffer", pos=(150, 200))
 
-    # connect nodes
-    fc.connectTerminals(dippid_node['accelX'], buffer_node_x['dataIn'])
-    fc.connectTerminals(dippid_node['accelY'], buffer_node_y['dataIn'])
-    fc.connectTerminals(dippid_node['accelZ'], buffer_node_z['dataIn'])
+    # connect buffer nodes
+    fc.connectTerminals(dippid_node["accelX"], buffer_node_x["dataIn"])
+    fc.connectTerminals(dippid_node["accelY"], buffer_node_y["dataIn"])
+    fc.connectTerminals(dippid_node["accelZ"], buffer_node_z["dataIn"])
+
+    # connect FFT nodes
+    fc.connectTerminals(buffer_node_x['dataOut'], fft_node['accelX'])
+    fc.connectTerminals(buffer_node_y['dataOut'], fft_node['accelY'])
+    fc.connectTerminals(buffer_node_z['dataOut'], fft_node['accelZ'])
+
 
 if __name__ == "__main__":
-    # TODO: register fft and svm nodes
-    # fclib.registerNodeType(NormalVectorNode, [("Data",)])
-    # fclib.registerNodeType(LogNode, [("Data",)])
+    fclib.registerNodeType(FftNode, [("FftNode",)])
+
     app = QtGui.QApplication([])
     win = QtGui.QMainWindow()
     win.setWindowTitle("DIPPID Activity Recognizer")
@@ -44,7 +85,10 @@ if __name__ == "__main__":
     # create DIPPID node
     dippid_node = fc.createNode("DIPPID", pos=(0, 0))
 
-    init_accel_nodes(layout, fc, dippid_node)
+    # create FFT node
+    fft_node = fc.createNode("FftNode", pos=(300, 100))
+
+    init_nodes(layout, fc, dippid_node, fft_node)
 
     win.show()
     if (sys.flags.interactive != 1) or not hasattr(QtCore, "PYQT_VERSION"):
