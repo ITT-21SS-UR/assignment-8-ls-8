a42dca5ceb306345f1240294edba1685e6233e28
michischmidt <schmidt.m.dev@gmail.com>
2021-06-22 10:46:18 +0200

    started activity ui

--- activity_recognizer.py
+++ activity_recognizer.py
@@ -2,3 +2,39 @@
 # coding: utf-8
 # -*- coding: utf-8 -*-
 # Script was written by Johannes Lorper and Michael Schmidt
+
+import sys
+from PyQt5 import QtGui, QtCore
+import pyqtgraph as pg
+from pyqtgraph.flowchart import Flowchart, Node
+from DIPPID_pyqtnode import DIPPIDNode, BufferNode
+import pyqtgraph.flowchart.library as fclib
+
+if __name__ == "__main__":
+    # fclib.registerNodeType(NormalVectorNode, [("Data",)])
+    # fclib.registerNodeType(LogNode, [("Data",)])
+    app = QtGui.QApplication([])
+    win = QtGui.QMainWindow()
+    win.setWindowTitle("DIPPID Graph")
+
+    # Define a top-level widget to hold everything
+    central_widget = QtGui.QWidget()
+    win.setCentralWidget(central_widget)
+
+    # Create a grid layout to manage the widgets size and position
+    layout = QtGui.QGridLayout()
+    central_widget.setLayout(layout)
+
+    # Creating flowchart
+    fc = Flowchart(terminals={})
+    layout.addWidget(fc.widget(), 0, 0, 2, 1)
+
+    # create DIPPID node
+    dippid_node = fc.createNode("DIPPID", pos=(0, 0))
+
+    # init_accel_plotting(layout, fc, dippid_node, log_node)
+    # init_normal_vector_plotting(layout, fc, dippid_node, log_node)
+
+    win.show()
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, "PYQT_VERSION"):
+        sys.exit(app.exec_())
