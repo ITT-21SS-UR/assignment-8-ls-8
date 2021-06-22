5ef8e04ea7d55214c274d76d5e7c85932bc39688
michischmidt <schmidt.m.dev@gmail.com>
2021-06-22 11:58:12 +0200

    connected accel nodes

--- activity_recognizer.py
+++ activity_recognizer.py
@@ -10,12 +10,24 @@ from pyqtgraph.flowchart import Flowchart, Node
 from DIPPID_pyqtnode import DIPPIDNode, BufferNode
 import pyqtgraph.flowchart.library as fclib
 
+def init_accel_nodes(layout, fc, dippid_node):
+    # create buffer nodes
+    buffer_node_x = fc.createNode("Buffer", pos=(150, 0))
+    buffer_node_y = fc.createNode("Buffer", pos=(150, 100))
+    buffer_node_z = fc.createNode("Buffer", pos=(150, 200))
+
+    # connect nodes
+    fc.connectTerminals(dippid_node['accelX'], buffer_node_x['dataIn'])
+    fc.connectTerminals(dippid_node['accelY'], buffer_node_y['dataIn'])
+    fc.connectTerminals(dippid_node['accelZ'], buffer_node_z['dataIn'])
+
 if __name__ == "__main__":
+    # TODO: register fft and svm nodes
     # fclib.registerNodeType(NormalVectorNode, [("Data",)])
     # fclib.registerNodeType(LogNode, [("Data",)])
     app = QtGui.QApplication([])
     win = QtGui.QMainWindow()
-    win.setWindowTitle("DIPPID Graph")
+    win.setWindowTitle("DIPPID Activity Recognizer")
 
     # Define a top-level widget to hold everything
     central_widget = QtGui.QWidget()
@@ -32,8 +44,7 @@ if __name__ == "__main__":
     # create DIPPID node
     dippid_node = fc.createNode("DIPPID", pos=(0, 0))
 
-    # init_accel_plotting(layout, fc, dippid_node, log_node)
-    # init_normal_vector_plotting(layout, fc, dippid_node, log_node)
+    init_accel_nodes(layout, fc, dippid_node)
 
     win.show()
     if (sys.flags.interactive != 1) or not hasattr(QtCore, "PYQT_VERSION"):
